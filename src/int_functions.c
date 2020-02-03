f231254be9f19e63ad0b073b0e937089aa4e9930
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-02-03 07:24:25 -0500

    Added C files for multiple files example

---
+++ src/int_functions.c
@@ -0,0 +1,16 @@
+
+
+#include "int_functions.h"
+
+int Sum(int a, int b){
+
+	// Hace la suma de dos números enteros
+	return a + b;
+}
+
+
+int Sub(int a, int b){
+
+	// Hace la resta de dos números enteros
+	return a - b;
+}
---
+++ src/multiple_files_main.c
@@ -0,0 +1,19 @@
+
+
+#include "int_functions.h"
+
+int main(){
+
+	int a, b, c, d;
+
+	a = 5;
+
+	b = 4;
+
+	c = Sum(a, b);
+
+	d = Sub(a, b);
+
+	return 0;
+
+}
