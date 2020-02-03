1a4eb9b158b4aff3b14860e049a4163b08f10225
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-02-03 07:44:41 -0500

    Integer operations source file

---
+++ src/int_functions.c
@@ -0,0 +1,11 @@
+#include <int_functions.h>
+
+int Sum(int a, int b){
+  // Performs an integer addition
+  return a + b;
+}
+
+int Sub(int a, int b){
+  // Performs an integer substraction
+  return a - b;
+}
